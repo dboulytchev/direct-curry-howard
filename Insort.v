6194b99e53f5328bd93b8c046edb020f5f0cc9aa
Podkopaev Anton <podkoav239@gmail.com>
2015-03-31 11:48:53 +0300

    Added proof for Insort lemma.

--- Insort.v
+++ Insort.v
@@ -35,10 +35,68 @@ Inductive is_permutation : list nat -> list nat -> Prop :=
 
 Hint Constructors is_permutation.
 
-(* TODO: prove *) 
+Lemma smallest_perm a n tl : is_smallest a (n :: a :: tl) -> is_smallest a (a :: n :: tl).
+Proof.
+intros H; inversion H.
+  rewrite H0 in H. assumption.
+  inversion H4.
+    apply (smallest_head a n [n] (lt_le_weak _ _ H3)). constructor.
+    set (le_lt_dec n m0) as S.
+      inversion S.
+        apply (smallest_head a n (n :: tl) (lt_le_weak _ _ H3)).                
+          apply (smallest_head n m0 tl H9 H8).
+        apply (smallest_head a m0 (n :: tl) H6).
+          apply (smallest_tail n m0 tl H9 H8).
+    omega.
+Qed.
+
+Lemma smallest_with_n a n tl : is_smallest a (a :: tl) -> a <= n -> is_smallest a (a :: n :: tl).
+Proof.
+intros H; inversion H; intros Hle.
+  apply (smallest_head a n [n] Hle). constructor.
+  apply (smallest_perm).
+    set (le_lt_eq_dec a n Hle) as S.
+      inversion S.
+        apply (smallest_tail _ _ _ H4 H).
+        rewrite<-H4.
+          apply (smallest_head a a (a :: tl) (le_refl a) H). 
+  omega.
+Qed.
+
+Lemma smallest_without_n a n tl : is_smallest a (a :: n :: tl) -> is_smallest a (a :: tl).
+Proof.
+intros H; inversion H.
+  inversion H3. 
+    apply smallest_unit. 
+    apply (smallest_head a m0 tl).
+      rewrite H4 in H3, H1.
+        apply (le_trans a n m0 H1 H7). assumption.
+    apply (smallest_head a m tl H1). assumption.
+  omega.
+Qed.
+
+Lemma smallest_than_snd a n tl : is_smallest a (a :: n :: tl) -> a <= n.
+Proof.
+intros H; inversion H.
+  inversion H3.
+    rewrite<-H6; assumption.
+    rewrite<-H4; assumption.
+    apply lt_le_weak.
+      apply (le_lt_trans a m n H1 H7).
+  omega.
+Qed.  
+
 Lemma insert_bigger : forall (a b : nat) (l l' : list nat),
   is_smallest a (a::l) -> b > a -> is_inserted b l l' -> is_smallest a (a::l').
-Proof. admit. Qed.  
+Proof.
+intros a b l l' H1 H2 H3.
+  induction H3.
+    apply (smallest_with_n a n tl H1); omega.
+    apply (smallest_with_n a m tl').
+      apply IHis_inserted; try assumption.
+        apply (smallest_without_n a m tl H1).       
+      apply (smallest_than_snd a m tl H1).
+Qed.
 
 Lemma insert_sorted : forall (a : nat) (l : list nat),
   is_sorted l -> {l' | is_inserted a l l' & is_sorted l'}.
